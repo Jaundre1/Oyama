64a453b531bf8df079d86af276dbbccf8241da01
juand <juandreslimis@gmail.com>
2024-09-28 14:17:09 +0200

    i need this one

--- app/src/main/java/com/example/oyama/MainActivity.kt
+++ app/src/main/java/com/example/oyama/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.google.firebase.FirebaseApp
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,12 +13,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Initialize Firebase
+        FirebaseApp.initializeApp(this)
+
         // Hide the action bar
         supportActionBar?.hide()
 
         // Set the status bar color
         window.statusBarColor = ContextCompat.getColor(this, android.R.color.white)
 
+        // Set up the selectDepotButton to navigate to SecondaryActivity
         val selectDepotButton: Button = findViewById(R.id.selectDepotButton)
         selectDepotButton.setOnClickListener {
             val intent = Intent(this, SecondaryActivity::class.java)
